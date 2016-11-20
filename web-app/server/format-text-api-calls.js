7fab59b28342d00e2e283201c55175de94f8d4fe
dbaruchel <dbaruchel@gmail.com>
2016-11-20 05:17:38 +0100

    more styling

--- web-app/server/format-text-api-calls.js
+++ web-app/server/format-text-api-calls.js
@@ -7,12 +7,14 @@ FormatText = {
     const imageUrlRequest = HTTP.post(url, {
       data: {
         // p2i_html: `<div style="width: 400px;"><div style="font-family: 'Abril Fatface';padding: 0.5rem;background-color: white;letter-spacing: 0.4rem;text-transform: uppercase;font-size: 0.8rem;color:#333;"># Que Dirait Diderot ?</div><div style="font-family: 'Abril Fatface';padding: 1.5rem;color:#333;background-color: #84f7fd;">"${text}"</div><div style="font-family: 'Abril Fatface';padding: 0rem 3rem 1rem;background-color: #84f7fd;text-transform: uppercase;font-size:0.8rem;color:#333;">${ref}</div></div>`,
-        p2i_html: encodeURIComponent('<div>Yolo</div>'),
+        p2i_html: '<div>Yolo</div>',
         p2i_key: 'ea8fe257f32c4b77',
       },
     });
     return imageUrlRequest;
   },
 };
+
+// FormatText.getImageUrl({ text:'Vois-tu, mon jeune Trump, tu \'es qu\'une nouvelle recrue. Ceci dit pour ton édification, ami Trump, laisse-moi ajouter que je ne plaisantais nullement en exprimant la pensée que nous ne reverrions peut-être plus le Sborg, notre illustre chef.', ref:'Les ravageurs de la mer - 1980 - Louis Jacolliot' });
 
 // FormatText.getImageUrl({ text:'Vois-tu, mon jeune Trump, tu \'es qu\'une nouvelle recrue. Ceci dit pour ton édification, ami Trump, laisse-moi ajouter que je ne plaisantais nullement en exprimant la pensée que nous ne reverrions peut-être plus le Sborg, notre illustre chef.', ref:'Les ravageurs de la mer - 1980 - Louis Jacolliot' });
