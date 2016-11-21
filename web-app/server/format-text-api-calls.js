658606d0b99db844a3503d39037d73afe5326df9
dbaruchel <dbaruchel@gmail.com>
2016-11-21 22:03:54 +0100

    better font for shareable cards

--- web-app/server/format-text-api-calls.js
+++ web-app/server/format-text-api-calls.js
@@ -6,7 +6,8 @@ FormatText = {
     const url = 'http://api.page2images.com/html2image';
     const imageUrlRequest = HTTP.post(url, {
       params: {
-        p2i_html: `<div style="width:800px;text-align:center;font-size:2rem;"><div style="font-family: 'Abril Fatface';padding: 0.5rem;background-color: white;letter-spacing: 0.4rem;text-transform: uppercase;font-size: 1.6rem;color:#333;"># Que Dirait Diderot ?</div><div style="font-family: 'Abril Fatface';padding: 3rem;color:#333;background-color: #84f7fd;">"${text}"</div><div style="font-family: 'Abril Fatface';padding: 0rem 6rem 2rem;background-color: #84f7fd;text-transform: uppercase;font-size:1.6rem;color:#333;">${title}</div></div>`,
+        p2i_html: `<html><head><link rel="stylesheet" type="text/css"
+          href="https://fonts.googleapis.com/css?family=Abril+Fatface"></head><body><div style="width:800px;text-align:center;font-size:2rem;font-family:'Abril Fatface'"><div style="padding: 0.5rem;background-color: white;letter-spacing: 0.4rem;text-transform: uppercase;font-size: 1.6rem;color:#333;"># Que Dirait Diderot ?</div><div style="padding: 3rem;color:#333;background-color: #84f7fd;">"${text}"</div><div style="padding: 0rem 6rem 2rem;background-color: #84f7fd;text-transform: uppercase;font-size:1.6rem;color:#333;">${title}</div></div></html>`,
         // p2i_html: '<html><div>Yolo</div></html>',
         p2i_key: 'ea8fe257f32c4b77',
         p2i_size: '2400x0',
@@ -18,6 +19,4 @@ FormatText = {
   },
 };
 
-// FormatText.getImageUrl({ text:'Vois-tu, mon jeune Trump, tu \'es qu\'une nouvelle recrue. Ceci dit pour ton édification, ami Trump, laisse-moi ajouter que je ne plaisantais nullement en exprimant la pensée que nous ne reverrions peut-être plus le Sborg, notre illustre chef.', ref:'Les ravageurs de la mer - 1980 - Louis Jacolliot' });
-
-// FormatText.getImageUrl({ text:'Vois-tu, mon jeune Trump, tu \'es qu\'une nouvelle recrue. Ceci dit pour ton édification, ami Trump, laisse-moi ajouter que je ne plaisantais nullement en exprimant la pensée que nous ne reverrions peut-être plus le Sborg, notre illustre chef.', ref:'Les ravageurs de la mer - 1980 - Louis Jacolliot' });
+// FormatText.getImageUrl({ text:'Vois-tu, mon jeune Trump, tu \'es qu\'une nouvelle recrue. Ceci dit pour ton édification, ami Trump, laisse-moi ajouter que je ne plaisantais nullement en exprimant la pensée que nous ne reverrions peut-être plus le Sborg, notre illustre chef.', title:'Les ravageurs de la mer - 1980 - Louis Jacolliot' });
